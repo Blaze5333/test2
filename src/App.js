8656b656a0c39d6ca2b60f8a6baa9ab4d69545e1
Blaze5333 <arwachaiwala@gmail.com>
2023-01-06 22:14:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello from frontend
+      
+    </div>
+  );
+}
+
+export default App;
